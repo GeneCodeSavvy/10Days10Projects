0363282bad48d70e7fac61c8349cd9074e475835
GeneCodeSavvy <hxrsh.09@gmail.com>
2024-04-02 04:15:14 +0530

    Day 1

---
+++ D1/README.md
@@ -0,0 +1,4 @@
+# Topics covered
+1. Print function
+2. Input function
+3. String Manipulation
---
+++ D1/band_name_generator.py
@@ -0,0 +1,9 @@
+def main(city:str, pet:str):
+    print(city+"_"+pet)
+
+if __name__ == "__main__":
+    print(" Welcome to band name generator :) ")
+    print(" I would like to ask two questions to generate a name for your band")
+    city_name : str = input(" What is the name of your city?")
+    pet_name : str = input(" What was your pet's name?")
+    main(city=city_name, pet=pet_name)
